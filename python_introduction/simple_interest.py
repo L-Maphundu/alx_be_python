5a01f36f2ee95686ae25102f3a314365107d3c09
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-07-09 15:46:35 +0200

    fixed

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,10 @@
 #This script is for calculating simple interest given principal (p) amount, interest rate (r) and time (t).
 
 def main():
-    p = 1000
-    r = 0.05
-    t = 3
-    print(f"The simple interest is: {interest(p, r,t)}")
+    principal = 1000
+    rate = 0.05
+    time = 3
+    print(f"The simple interest is: {interest(principal, rate,time)}")
 
 def interest(principal: float,rate: float,time: float) -> float:
     """Calculates interest amount given: principal amount(p),interest rate (r) and time (t)"""
