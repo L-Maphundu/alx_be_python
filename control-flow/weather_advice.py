e10f7f5b4d697735c58855ca5f1ccf466443bb66
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-07-14 19:52:35 +0200

    weather advice.

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,17 @@
+#This script is for recommendation for what to wear based on what the user is.
+
+def main():
+    weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+    print(what_to_wear(weather))
+
+def what_to_wear(wthr: str) -> str:
+    """Returns a recommendation for what to wear based on whether the weather (wthr)
+    is sunny, rainy or cold."""
+    match wthr:
+        case 'sunny': return "Wear a t-shirt and sunglasses."
+        case 'rainy': return "Don't forget your umbrella and a raincoat."
+        case 'cold': return "Make sure to wear a warm coat and a scarf."
+        case _: return "Sorry, I don't have recommendations for this weather."
+
+
+main()
