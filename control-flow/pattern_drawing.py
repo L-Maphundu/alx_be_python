ebc82f069154263f06829c8d2131f2488eb6c503
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-07-14 20:59:33 +0200

    squares...

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+#This script will prompt the user to enter a positive integer, then use nested loops to print a square pattern of that size made of asterisks (*).
+
+size = int(input("Enter the size of the pattern: "))
+row = 0
+
+while row < size:
+    for i in range(size):
+        print("*", end="")
+    print()
+    row += 1
+    
+    
