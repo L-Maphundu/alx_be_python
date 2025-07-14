7ba7bab3a0a730d0e426f266fa7b3d3d54814ee4
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-07-14 20:47:01 +0200

    times table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -4,9 +4,9 @@ def main():
     number = int(input("Enter a number to see its multiplication table: "))
     times_table(number)
 
-def times_table(num):
+def times_table(number):
     """Returns a multiplicaiton table for any given positive integer."""
     for i in range(1, 11):
-        print(f"{num} * {i} = {num*i}")
+        print(f"{number * i} = {number*i}")
 
 main()
