a33aa6d3492f87809ab7e461219925949aada592
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-07-25 22:21:42 +0200

    temprature converter

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,8 +2,20 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
+def convert_to_celsius(fahrenheit):
+    tmp = FAHRENHEIT_TO_CELSIUS_FACTOR*(fahrenheit - 32)
+    return tmp
+def convert_to_fahrenheit(celsius):
+    tmp = CELSIUS_TO_FAHRENHEIT_FACTOR*celsius + 32
+    return tmp
+
 def main():
-    temp = float(input("Enter the temperature to convert: "))
+    temp_str = input("Enter the temperature to convert: ").strip()
+    if temp_str.isnumeric:
+        temp = int(temp_str)
+    else:
+        print("Invalid temperature. Please enter a numeric value."
+              )
     current_unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().lower()
 
     if current_unit == 'f':
@@ -11,16 +23,7 @@ def main():
     elif current_unit == 'c':
         result = f"{temp}°C is {convert_to_fahrenheit(temp)}°F"
     else:
-        result = "Unrecognised temperature units"
+        result = "Please make sure the temperature units are in celsius (C) or fahrenheit (F)"
     print(result)
-
-
-
-
-def convert_to_celsius(fahrenheit):
-    tmp = FAHRENHEIT_TO_CELSIUS_FACTOR*(fahrenheit - 32)
-    return tmp
-def convert_to_fahrenheit(celsius):
-    tmp = CELSIUS_TO_FAHRENHEIT_FACTOR*celsius + 32
-    return tmp
+    
 main()
