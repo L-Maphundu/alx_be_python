967bf8fee7ad77fea6400b92e8b79240b717e064
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-07-25 17:46:40 +0200

    arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+#contains a function that can perform all four basic arithmetic operations.
+
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == "add":
+        return (num1 + num2)
+    elif operation == "subtract":
+        return (num1 - num2)
+    elif operation == "multiply":
+        return (num1*num2)
+    else:
+        if num2 != 0:
+            return (num1/num2)
+        else:
+            return "The second number (denominator) cannot be 0!"               
+    
