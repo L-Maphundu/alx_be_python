a1898a957e7a4e6ebe37f21a692b26da7d2071ac
Luyolo Maphundu <luyolomaphundu@gmail.com>
2025-07-26 20:53:12 +0200

    temperature conversion

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -4,7 +4,7 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     global FAHRENHEIT_TO_CELSIUS_FACTOR
-    return -32 + fahrenheit*FAHRENHEIT_TO_CELSIUS_FACTOR 
+    return (fahrenheit - 32)*FAHRENHEIT_TO_CELSIUS_FACTOR 
 
 def convert_to_fahrenheit(celsius):
     global CELSIUS_TO_FAHRENHEIT_FACTOR
